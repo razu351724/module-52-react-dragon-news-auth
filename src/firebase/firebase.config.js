5d7237fb158a7bff81a2128f1be9346255d101c5
RAZU-sheikh <razu351724@gmail.com>
2023-10-03 10:05:21 +0600

    firebase and satap

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7P2F7wXCNdEU9lB2RdA-Pb632qde73LA",
+  authDomain: "module-52-react-dragon-news.firebaseapp.com",
+  projectId: "module-52-react-dragon-news",
+  storageBucket: "module-52-react-dragon-news.appspot.com",
+  messagingSenderId: "1077245910281",
+  appId: "1:1077245910281:web:296ab362ddd80f068747d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
