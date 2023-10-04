669b0bfcd3c908a29d2efe148def5010337d6770
RAZU-sheikh <razu351724@gmail.com>
2023-10-05 04:23:02 +0600

    Load news and create news datail page: midele section

---
+++ src/pages/Home/NewsCard.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'; 
+import { Link } from 'react-router-dom';
+
+const NewsCard = ({ news }) => {
+    const { title, image_url, details, _id } = news;
+
+    return (
+        <div className="card  bg-base-100 shadow-xl mb-16">
+            <figure><img src={image_url} alt="#" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                {
+                    details.length > 200? 
+                    <p>{details.slice(0,200)} <Link className='text-blue-500 font-bold'
+                    to={`/news/${_id}`}
+                    >Read More....</Link></p>
+                    :
+                    <p>{details}</p>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default NewsCard;
+
+NewsCard.propTypes = {
+    news: PropTypes.node
+}
---
+++ src/pages/News/News.jsx
@@ -0,0 +1,9 @@
+const News = () => {
+    return (
+        <div>
+            <h2>the is news page</h2>
+        </div>
+    );
+};
+
+export default News;
