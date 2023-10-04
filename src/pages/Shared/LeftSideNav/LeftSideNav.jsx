edf270c4158dc7526a604cc0a1a17ec8feb03ba7
RAZU-sheikh <razu351724@gmail.com>
2023-10-04 09:45:34 +0600

    Left side navigation and public holdera data lode or use

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+
+    const [categories, setcategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setcategories(data))
+    },[])
+
     return (
-        <div>
-            <h2 className="text-2xl">Left side</h2>
+        <div className="space-y-4">
+            <h2 className="text-2xl">All Categorie</h2>
+            {
+                categories.map(category => <Link
+                className="block ml-4 text-xl font-semibold "
+                key={category.id}
+                to={`/category/${category.id}`}
+                >{category.name}</Link>)
+            }
         </div>
     );
 };
